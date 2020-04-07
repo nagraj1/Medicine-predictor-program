6a7650ed914924bf72717f949ce0ed609ad23487
nagraj1 <63263949+nagraj1@users.noreply.github.com>
2020-04-07 22:48:26 +0530

    I python

---
+++ medicine.py
@@ -0,0 +1,22 @@
+str=('cholera','tuberculosis','plague','anthrax')
+print(str)
+disease=input('Enter the name of the disease\n')
+
+if disease=='cholera':
+    medicine('vaxchora')
+    print('medicine',medicine)
+
+
+elif disease=='tuberculosis':
+      medicine=('Bacille calmette Guerin')
+      print('medicine',medicine)
+
+elif disease=='plague':
+      medicine=('cefuroxime')
+      print('medicine',medicine)
+
+      
+
+elif disease=='Anthrax':
+      medicine=('doxycycline')
+      print('medicine',medicine)
